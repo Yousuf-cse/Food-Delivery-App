3bd289b68e2ffa55c8e66cd82e4e5624ec978348
Yousuf <yousuf.mallik79@gmail.com>
2025-05-07 00:50:02 +0530

    Add backend structure with initial routes, controllers, and error handling

---
+++ backend/src/app.js
@@ -0,0 +1,21 @@
+import express from "express";
+const app = express();
+import cors from "cors";
+import exampleRouter from "./routes/example.routes.js";
+import globalErrorHandler from "./middlewares/globalErrorHandler.js";
+
+app.use(cors({
+    origin: process.env.CORS_ORIGIN,
+    credentials: true
+}));
+app.use(express.json({limit: "16kb"}))
+app.use(express.urlencoded({extended: true, limit: "16kb"}));
+
+
+app.get("/", (req, res) => {
+  res.send("Hello from the backend server!");
+});
+app.use("/example", exampleRouter);
+app.use(globalErrorHandler);
+
+export {app};
