3befecb00f631ac733ca4f819a89c33410a76639
Yousuf-cse <yousuf.mallik79@gmail.com>
2025-05-17 00:16:55 +0530

    Remove example routes file as it is no longer needed

--- backend/src/routes/example.routes.js
+++
@@ -1,8 +0,0 @@
-import express from "express";
-import { getExample } from "../controllers/example.controller.js";
-
-const router = express.Router();
-
-router.get("/", getExample);
-
-export default router;
