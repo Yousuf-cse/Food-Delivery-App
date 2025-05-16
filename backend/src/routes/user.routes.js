40e206c721101e0a1653904130978fe42ec259a9
Yousuf-cse <yousuf.mallik79@gmail.com>
2025-05-17 00:14:34 +0530

    Add user routes for registration, login, logout, and token refresh

---
+++ backend/src/routes/user.routes.js
@@ -0,0 +1,15 @@
+import Router from "express";
+import { loginUser, logoutUser, registerUser, refreshAccessToken } from "../controllers/User.controller";
+import { verifyJWT } from "../middlewares/auth.middleware";
+
+const router = Router();
+
+router.route("/register").post(registerUser)
+
+router.route("/login").post(loginUser)
+
+router.route("/logout").post(verifyJWT, logoutUser)
+
+router.route("/refresh-token").post(refreshAccessToken)
+
+export default router;  
