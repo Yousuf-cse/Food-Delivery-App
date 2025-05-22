166abe3fd8482db4f2288ca021cf670c5243b36b
Yousuf <yousuf.mallik79@gmail.com>
2025-05-22 12:00:26 +0530

    Update JSON body limit and correct user router API path

--- backend/src/app.js
+++ backend/src/app.js
@@ -9,7 +9,7 @@ app.use(cors({
     origin: process.env.CORS_ORIGIN,
     credentials: true
 }));
-app.use(express.json({limit: "16kb"}))
+app.use(express.json({limit: "24kb"}))
 app.use(express.urlencoded({extended: true, limit: "16kb"}));
 app.use(cookieParser());
 
@@ -18,7 +18,7 @@ app.get("/", (req, res) => {
   res.send("Hello from the backend server!");
 });
 
-app.use("/api/v1/user", userRouter);
+app.use("/api/user", userRouter);
 
 app.use(globalErrorHandler);
 
